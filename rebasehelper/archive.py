0f82aeb3283c9db600f1210ee5efe28f5c3044d9
Petr Hracek <phracek@redhat.com>
2013-09-11 13:16:44 +0200

    Added support for LZMA

--- rebasehelper/archive.py
+++ rebasehelper/archive.py
@@ -2,21 +2,30 @@
 
 import tarfile
 import zipfile
+import lzma
+from rebasehelper import settings
+from rebasehelper.logger import logger
 
 class Archive(object):
     """ Class representing an archive with sources """
-
+    filename = None
+    archive = None
     def __init__(self, filename):
-        if tarfile.is_tarfile(filename):
-            self._open = tarfile.TarFile.open
+        self.filename = filename
+        if self.filename.endswith(".tar.xz"):
+            xz_file = lzma.LZMAFile(self.filename,"r")
+            self.archive = tarfile.open(mode='r',fileobj=xz_file)
+        elif self.filename.endswith(".tar.bz2") or self.filename.endswith("tar.gz"):
+            self.archive = tarfile.TarFile.open(self.filename)
         elif zipfile.is_zipfile(filename):
             self._open = zipfile.ZipFile
         else:
             raise NotImplementedError("Unsupported archive type")
         self._filename = filename
 
-    def extract(self, path="."):
+    def extract(self, path=settings.TEMPLATE_DIR):
         """ Extracts the archive into the given path """
-        archive = self._open(self._filename)
-        archive.extractall(path)
-        archive.close()
+        logger.info("extracting {0}".format(self.filename))
+        self.archive.extractall(path)
+        self.archive.close()
+
