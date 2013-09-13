8c52a810da5f15a6fea7d62271f2267cf5901240
Tomas Hozza <thozza@redhat.com>
2013-09-13 14:21:23 +0200

    Add ProcessHelper.run_subprocess_cwd() to run commands in different working dir
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- rebasehelper/utils.py
+++ rebasehelper/utils.py
@@ -3,9 +3,13 @@
 import subprocess
 
 class ProcessHelper(object):
-    
+
     @staticmethod
     def run_subprocess(cmd, output=None):
+        return ProcessHelper.run_subprocess_cwd(cmd, output=output)
+
+    @staticmethod
+    def run_subprocess_cwd(cmd, cwd=None, output=None):
         if output is not None:
             out_file = open(output, "w")
         else:
@@ -13,6 +17,7 @@ class ProcessHelper(object):
         sp = subprocess.Popen(cmd,
                               stdout=subprocess.PIPE,
                               stderr=subprocess.STDOUT,
+                              cwd=cwd,
                               shell=False)
         for line in sp.stdout:
             if out_file is not None:
