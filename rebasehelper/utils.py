eb0665dadd5f859e2f4b43d8c89795952efa051c
Tomas Hozza <thozza@redhat.com>
2013-09-13 11:20:57 +0200

    Modify run_subprocess() to be able to write process output to file
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- rebasehelper/utils.py
+++ rebasehelper/utils.py
@@ -2,16 +2,22 @@
 
 import subprocess
 
-def run_subprocess(cmd):
-    sp = subprocess.Popen(cmd,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT,
-                          shell=False)
-
-    while True:
-        lines = sp.stdout.readline()
-        if lines == "":
-            break
-        print lines,
-    sp.wait()
-    return sp.returncode
+class ProcessHelper(object):
+    
+    @staticmethod
+    def run_subprocess(cmd, output=None):
+        if output is not None:
+            out_file = open(output, "w")
+        else:
+            out_file = None
+        sp = subprocess.Popen(cmd,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT,
+                              shell=False)
+        for line in sp.stdout:
+            if out_file is not None:
+                out_file.write(line)
+            else:
+                print line.rstrip("\n")
+        sp.wait()
+        return sp.returncode
