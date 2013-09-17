2d3dba6973dfbc5745952edfcfffca3632a2e6d5
Petr Hracek <phracek@redhat.com>
2013-09-17 21:03:19 +0200

    Patch checker for running checks with patches

---
+++ rebasehelper/patch_checker.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from rebasehelper import rebase_base
+
+class PatchChecker(rebase_base.RebaseBase):
+    """ Class used for testing and other future stuff, ...
+        Each method should overwrite method like run_check
+    """
+
+    def run_check(self, **kwargs):
+        """
+            Method will check all patches in relevant package
+        """
+        return []
