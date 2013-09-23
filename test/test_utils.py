094165f26abe873de61b2d813500c2658b226df5
Tomas Hozza <thozza@redhat.com>
2013-09-23 17:23:39 +0200

    Add unit tests for PathHelper methods
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,12 +4,14 @@ import os
 import tempfile
 import shutil
 from rebasehelper.utils import ProcessHelper
+from rebasehelper.utils import PathHelper
+
 
 class TestProcessHelper(object):
-    """ProcessHelper tests"""
+    """ ProcessHelper tests """
 
     class TestRunSubprocessCwd(object):
-        """ProcessHelper - run_subprocess_cwd() tests"""
+        """ ProcessHelper - run_subprocess_cwd() tests """
         TEMP_FILE = "temp_file"
         TEMP_DIR = "temp_dir"
         OUT_FILE = "output_file"
@@ -57,3 +59,97 @@ class TestProcessHelper(object):
             assert os.path.exists(os.path.join(self.TEMP_DIR, self.TEMP_FILE))
             assert os.path.exists(self.OUT_FILE)
             assert open(self.OUT_FILE).readline().strip("\n") == self.TEMP_FILE
+
+
+class TestPathHelper(object):
+    """ PathHelper tests """
+
+    class TestPathHelperFindBase(object):
+        """ Base class for find methods """
+        WORKING_DIR = tempfile.gettempdir()
+        dirs = ["dir1",
+                "dir1/foo",
+                "dir1/faa",
+                "dir1/foo/bar",
+                "dir1/foo/baz",
+                "dir1/bar",
+                "dir1/baz",
+                "dir1/baz/bar"]
+        files = ["file",
+                 "ffile",
+                 "ppythooon",
+                 "dir1/fileee",
+                 "dir1/faa/pythooon",
+                 "dir1/foo/pythooon",
+                 "dir1/foo/bar/file",
+                 "dir1/foo/baz/file",
+                 "dir1/baz/ffile",
+                 "dir1/bar/file",
+                 "dir1/baz/bar/ffile"]
+
+        def setup(self):
+            self.WORKING_DIR = tempfile.mkdtemp(prefix="rebase-helper-test-")
+            os.chdir(self.WORKING_DIR)
+            for d in self.dirs:
+                os.mkdir(d)
+            for f in self.files:
+                with open(f, "w") as fd:
+                    fd.write(f)
+
+        def teardown(self):
+            os.chdir(tempfile.gettempdir())
+            shutil.rmtree(self.WORKING_DIR)
+            self.WORKING_DIR = tempfile.gettempdir()
+
+    class TestFindFirstDirWithFile(TestPathHelperFindBase):
+        """ PathHelper - find_first_dir_with_file() tests """
+        def test_find_file(self):
+            assert PathHelper.find_first_dir_with_file(
+                "dir1", "file") == os.path.abspath(
+                os.path.dirname(self.files[9]))
+            assert PathHelper.find_first_dir_with_file(
+                ".", "file") == os.path.abspath(os.path.dirname(self.files[0]))
+            assert PathHelper.find_first_dir_with_file(
+                "dir1/baz", "file") is None
+
+        def test_find_ffile(self):
+            assert PathHelper.find_first_dir_with_file(
+                "dir1", "*le") == os.path.abspath(
+                os.path.dirname(self.files[8]))
+            assert PathHelper.find_first_dir_with_file(
+                "dir1", "ff*") == os.path.abspath(
+                os.path.dirname(self.files[8]))
+            assert PathHelper.find_first_dir_with_file(
+                "dir1/foo", "ff*") is None
+
+        def test_find_pythoon(self):
+            assert PathHelper.find_first_dir_with_file(
+                "dir1", "pythooon") == os.path.abspath(
+                os.path.dirname(self.files[4]))
+            assert PathHelper.find_first_dir_with_file(
+                ".", "py*n") == os.path.abspath(os.path.dirname(self.files[4]))
+            assert PathHelper.find_first_dir_with_file(
+                "dir1/bar", "pythooon") is None
+
+    class TestFindFirstFile(TestPathHelperFindBase):
+        """ PathHelper - find_first_file() tests """
+        def test_find_file(self):
+            assert PathHelper.find_first_file(
+                "dir1", "file") == os.path.abspath(self.files[9])
+            assert PathHelper.find_first_file(
+                ".", "file") == os.path.abspath(self.files[0])
+            assert PathHelper.find_first_file("dir1/baz", "file") is None
+
+        def test_find_ffile(self):
+            assert PathHelper.find_first_file(
+                "dir1", "*le") == os.path.abspath(self.files[8])
+            assert PathHelper.find_first_file(
+                "dir1", "ff*") == os.path.abspath(self.files[8])
+            assert PathHelper.find_first_file("dir1/foo", "ff*") is None
+
+        def test_find_pythoon(self):
+            assert PathHelper.find_first_file(
+                "dir1", "pythooon") == os.path.abspath(self.files[4])
+            assert PathHelper.find_first_file(
+                ".", "py*n") == os.path.abspath(self.files[4])
+            assert PathHelper.find_first_file("dir1/bar", "pythooon") is None
