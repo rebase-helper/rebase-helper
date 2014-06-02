c91cca0eddae82fbfa600d4b3886ac76aabef67e
Tomas Hozza <thozza@redhat.com>
2014-06-02 17:39:02 +0200

    BuildHelper: add mock and rpmbuild classes drafts
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- rebasehelper/build_helper.py
+++ rebasehelper/build_helper.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from rebasehelper.utils import ProcessHelper
+from rebasehelper.utils import PathHelper
+from rebasehelper import settings
 from rebasehelper import logger
 
 build_tools = {}
@@ -31,24 +34,114 @@ class BuildToolBase(object):
 class MockBuildTool(BuildToolBase):
     """ Mock build tool. """
     CMD = "mock"
-    
+
     @classmethod
     def match(cls, cmd=None):
         if cmd == cls.CMD:
             return True
         else:
             return False
-            
+
+
     @classmethod
-    def build(cls):
+    def _build_srpm(cls, **kwargs):
+        """ Build SRPM using mock. """
+        spec = kwargs.get('spec')
+        sources = kwargs.get('sources')
+        root = kwargs.get('root')
+        arch = kwargs.get('arch')
+        resultdir = kwargs.get('resultdir')
+        output = kwargs.get('output')
+
+        cmd = [cls.CMD, '--buildsrpm', '--spec', spec, '--sources', sources,
+               '--resultdir', resultdir]
+        if root is not None:
+            cmd.extend(['--root', root])
+        if arch is not None:
+            cmd.extend(['--arch', arch])
+        ret = ProcessHelper.run_subprocess(cmd, output)
+
+        if ret != 0:
+            logger.debug("MockBuildTool: running '" + cmd + "' failed")
+            return None
+        else:
+            return PathHelper.find_first_file(resultdir, '*.\.src\.rpm')
+
+
+    @classmethod
+    def _build_rpm(cls, **kwargs):
+        """ Build RPM using mock. """
+        srpm = kwargs.get('srpm')
+        root = kwargs.get('root')
+        arch = kwargs.get('arch')
+        resultdir = kwargs.get('resultdir')
+        output = kwargs.get('output')
+
+        cmd = [cls.CMD, '--rebuild', srpm, '--resultdir', resultdir]
+        if root is not None:
+            cmd.extend(['--root', root])
+        if arch is not None:
+            cmd.extend(['--arch', arch])
+        ret = ProcessHelper.run_subprocess(cmd, output)
+
+        if ret != 0:
+            logger.debug("MockBuildTool: running '" + cmd + "' failed")
+            return None
+        else:
+            # TODO: implement the logic of returinig list of binary RPMs
+            raise NotImplementedError()
+
+
+    @classmethod
+    def build(cls, **kwargs):
+        """ Build the SRPM + RPM """
+        srpm = cls._build_srpm(kwargs)
+        if srpm is None:
+            logger.debug("MockBuildTool: Building SRPM failed")
+            raise RuntimeError()
+        return cls._build_rpm(srpm=srpm, kwargs)
+
+
+@register_build_tool
+class RpmbuildBuildTool(BuildToolBase):
+    """ rpmbuild build tool. """
+    CMD = "rpmbuild"
+
+    @classmethod
+    def match(cls, cmd=None):
+        if cmd == cls.CMD:
+            return True
+        else:
+            return False
+
+
+    @classmethod
+    def _build_srpm(cls, **kwargs):
+        """ Build SRPM using rpmbuild. """
         raise NotImplementedError()
+
+
+    @classmethod
+    def _build_rpm(cls, **kwargs):
+        """ Build RPM using rpmbuild. """
+        raise NotImplementedError()
+
+
+    @classmethod
+    def build(cls, **kwargs):
+        """ Build the SRPM + RPM """
+        srpm = cls._build_srpm(kwargs)
+        if srpm is None:
+            logger.debug("RpmbuildBuildTool: Building SRPM failed")
+            raise RuntimeError()
+        return cls._build_rpm(srpm=srpm, kwargs)
 
 
 class Builder(object):
     """
     Class representing a process of building binaries from sources.
     """
-    
+
     @classmethod
     def __init__(cls, tool=None):
         if tool is None:
@@ -62,10 +155,9 @@ class Builder(object):
 
         if cls._tool is None:
             raise NotImplementedError("Unsupported build tool")
-    
+
     @classmethod
-    def build(cls, spec_file=None, sources=None):
+    def build(cls, **kwargs):
         """ Build sources. """
-        logger.debug("Builder: Building sources '{0}' using '{1}'".format(
-                     sources, cls._tool_name))
-        raise NotImplementedError()
+        logger.debug("Builder: Building sources...")
+        return cls._tool.build(kwargs)
