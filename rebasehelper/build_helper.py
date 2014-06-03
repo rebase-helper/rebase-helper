1f450c848ee6619455b5ccab725fcec4a03b8b58
Tomas Hozza <thozza@redhat.com>
2014-06-03 13:19:45 +0200

    build_helper: Working MockBuildTool
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- rebasehelper/build_helper.py
+++ rebasehelper/build_helper.py
@@ -2,7 +2,6 @@
 
 from rebasehelper.utils import ProcessHelper
 from rebasehelper.utils import PathHelper
-from rebasehelper import settings
 from rebasehelper.logger import logger
 
 build_tools = {}
@@ -59,13 +58,15 @@ class MockBuildTool(BuildToolBase):
             cmd.extend(['--root', root])
         if arch is not None:
             cmd.extend(['--arch', arch])
+
+        logger.debug("MockBuildTool: running '" + str(cmd) + "'")
         ret = ProcessHelper.run_subprocess(cmd, output)
 
         if ret != 0:
-            logger.debug("MockBuildTool: running '" + cmd + "' failed")
+            logger.debug("MockBuildTool: running '" + str(cmd) + "' failed")
             return None
         else:
-            return PathHelper.find_first_file(resultdir, '*.\.src\.rpm')
+            return PathHelper.find_first_file(resultdir, '*.src.rpm')
 
 
     @classmethod
@@ -82,24 +83,41 @@ class MockBuildTool(BuildToolBase):
             cmd.extend(['--root', root])
         if arch is not None:
             cmd.extend(['--arch', arch])
+
+        logger.debug("MockBuildTool: running '" + str(cmd) + "'")
         ret = ProcessHelper.run_subprocess(cmd, output)
 
         if ret != 0:
-            logger.debug("MockBuildTool: running '" + cmd + "' failed")
+            logger.debug("MockBuildTool: running '" + str(cmd) + "' failed")
             return None
         else:
-            # TODO: implement the logic of returinig list of binary RPMs
-            raise NotImplementedError()
+            return [ f for f in PathHelper.find_all_files(resultdir, '*.rpm') if not f.endswith('.src.rpm') ]
 
 
     @classmethod
     def build(cls, **kwargs):
-        """ Build the SRPM + RPM """
-        srpm = cls._build_srpm(kwargs)
+        """ Builds the SRPM and RPM using mock
+
+        Keyword arguments:
+        spec -- path to a SPEC file
+        sources -- path to a DIR with SOURCES
+        root -- mock root used for building
+        arch -- architecture to build the RPM for
+        resultdir -- path to DIR where results should be stored
+        output -- where mock output should be stored (TODO: split log for RPM and SRPM)
+
+        Returns:
+        dict with:
+        'srpm' -> path to SRPM
+        'rpm' -> list with paths to RPMs
+        """
+        srpm = cls._build_srpm(**kwargs)
         if srpm is None:
             logger.debug("MockBuildTool: Building SRPM failed")
             raise RuntimeError()
-        return cls._build_rpm(kwargs, srpm=srpm)
+        rpms = cls._build_rpm(srpm=srpm, **kwargs)
+        return {'srpm': srpm,
+                'rpm': rpms}
 
 
 @register_build_tool
@@ -130,11 +148,11 @@ class RpmbuildBuildTool(BuildToolBase):
     @classmethod
     def build(cls, **kwargs):
         """ Build the SRPM + RPM """
-        srpm = cls._build_srpm(kwargs)
+        srpm = cls._build_srpm(**kwargs)
         if srpm is None:
             logger.debug("RpmbuildBuildTool: Building SRPM failed")
             raise RuntimeError()
-        return cls._build_rpm(kwargs, srpm=srpm)
+        return cls._build_rpm(srpm=srpm, **kwargs)
 
 
 class Builder(object):
@@ -142,22 +160,25 @@ class Builder(object):
     Class representing a process of building binaries from sources.
     """
 
-    @classmethod
-    def __init__(cls, tool=None):
+    def __init__(self, tool=None):
         if tool is None:
             raise TypeError("Expected argument 'tool' (pos 1) is missing")
-        cls._tool_name = tool
-        cls._tool = None
+        self._tool_name = tool
+        self._tool = None
 
         for build_tool in build_tools.values():
-            if build_tool.match(cls._tool_name):
-                cls._tool = build_tool
+            if build_tool.match(self._tool_name):
+                self._tool = build_tool
 
-        if cls._tool is None:
+        if self._tool is None:
             raise NotImplementedError("Unsupported build tool")
 
-    @classmethod
-    def build(cls, **kwargs):
+
+    def __str__(self):
+        return "<Builder tool_name='{_tool_name}' tool={_tool}>".format(**vars(self))
+
+
+    def build(self, **kwargs):
         """ Build sources. """
         logger.debug("Builder: Building sources...")
-        return cls._tool.build(kwargs)
+        return self._tool.build(**kwargs)
