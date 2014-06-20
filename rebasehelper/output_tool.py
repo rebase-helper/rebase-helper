41660cea5d17f83acb7b02c13319e1d1b7c3e9a6
Petr Hracek <phracek@redhat.com>
2014-06-20 12:11:53 +0200

    Remove BaseOutputTool class to output_tool.py module

--- rebasehelper/output_tool.py
+++ rebasehelper/output_tool.py
@@ -24,6 +24,17 @@ def check_output_argument(output_tool):
         sys.exit(0)
 
 
+class BaseOutputTool(object):
+    """ Class used for testing and other future stuff, ...
+        Each method should overwrite method like run_check
+    """
+
+    def print_summary(self, **kwargs):
+        """ Return list of files which has been changed against old version
+        This will be used by checkers
+        """
+        raise NotImplementedError()
+
 @register_build_tool
 class TextOutputTool(BaseOutputTool):
     """ Text output tool. """
