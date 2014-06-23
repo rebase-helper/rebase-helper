925288a7e0f1d7cee36047ceedcbaef20a093c9e
Tomas Hozza <thozza@redhat.com>
2014-06-23 08:35:49 +0200

    BaseChecker: Modify to correspond with all base classes
    Use classmethod decorator for BaseChecker methods.
    Add match method.
    Add dictionaty with check_tools
    Add decorator to add new check_tools
    
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- rebasehelper/base_checker.py
+++ rebasehelper/base_checker.py
@@ -2,14 +2,28 @@
 import os
 from rebasehelper.logger import logger
 
+check_tools = {}
+
+def register_check_tool(check_tool):
+    check_tools[check_tool.CMD] = check_tool
+    return check_tool
+
 
 class BaseChecker(object):
-    """ Class used for testing and other future stuff, ...
-        Each method should overwrite method like run_check
-    """
+    """ Base class used for testing tool run on final pkgs. """
 
-    def run_check(self, **kwargs):
-        """ Return list of files which has been changed against old version
-        This will be used by checkers
+    @classmethod
+    def match(cls, *args, **kwargs):
+        """
+        Checks if the tool name match the class implementation. If yes, returns
+        True, otherwise returns False.
         """
         raise NotImplementedError()
+
+    @classmethod
+    def run_check(cls, *args, **kwargs):
+        """
+        Perform the check itself and return results.
+        """
+        raise NotImplementedError()
+
