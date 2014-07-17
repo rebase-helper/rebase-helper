28aafe79163119580475cc582fd4cd062d6010ee
Tomas Hozza <thozza@redhat.com>
2014-07-17 09:34:19 +0200

    Add BuildTemporaryEnvironment class
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- rebasehelper/build_helper.py
+++ rebasehelper/build_helper.py
@@ -22,6 +22,7 @@ import os
 
 from rebasehelper.utils import ProcessHelper
 from rebasehelper.utils import PathHelper
+from rebasehelper.utils import TemporaryEnvironment
 from rebasehelper.logger import logger
 
 build_tools = {}
@@ -30,6 +31,70 @@ build_tools = {}
 def register_build_tool(build_tool):
     build_tools[build_tool.CMD] = build_tool
     return build_tool    
+
+class BuildTemporaryEnvironment(TemporaryEnvironment):
+    """
+    Class representing temporary environment for MockBuildTool.
+    """
+
+    TEMPDIR_SOURCES = TemporaryEnvironment.TEMPDIR + '_SOURCES'
+    TEMPDIR_SPEC = TemporaryEnvironment.TEMPDIR + '_SPEC'
+    TEMPDIR_SPECS = TemporaryEnvironment.TEMPDIR + '_SPECS'
+    TEMPDIR_RESULTS = TemporaryEnvironment.TEMPDIR + '_RESULTS'
+
+    def __init__(self, sources, patches, spec, results_dir):
+        super(BuildTemporaryEnvironment, self).__init__(self._build_env_exit_callback)
+        self._env['results_dir'] = results_dir
+        self.sources = sources
+        self.patches = patches
+        self.spec = spec
+
+    def __enter__(self):
+        obj = super(BuildTemporaryEnvironment, self).__enter__()
+        # create the directory structure
+        self._create_directory_sctructure()
+        # copy sources
+        for source in self.sources:
+            logger.debug("BuildTemporaryEnvironment: copying '{0}' to '{1}'".format(source,
+                                                                                    self._env[self.TEMPDIR_SOURCES]))
+            shutil.copy(source, self._env[self.TEMPDIR_SOURCES])
+        # copy patches
+        for patch in self.patches:
+            logger.debug("BuildTemporaryEnvironment: copying '{0}' to '{1}'".format(patch,
+                                                                                    self._env[self.TEMPDIR_SOURCES]))
+            shutil.copy(patch, self._env[self.TEMPDIR_SOURCES])
+        # copy SPEC file
+        spec_name = os.path.basename(self.spec)
+        self._env[self.TEMPDIR_SPEC] = os.path.join(self._env[self.TEMPDIR_SPECS], spec_name)
+        shutil.copy(self.spec, self._env[self.TEMPDIR_SPEC])
+        logger.debug("BuildTemporaryEnvironment: Copying '{0}' to '{1}'".format(self.spec,
+                                                                                self._env[self.TEMPDIR_SPEC]))
+
+        return obj
+
+    def _create_directory_sctructure(self):
+        """
+        Function creating the directory structure in the TemporaryEnvironment.
+        """
+        raise NotImplementedError('The create directory function has to be implemented in child class!')
+
+    def _build_env_exit_callback(self, results_dir, **kwargs):
+        """
+        The function that is called just before the destruction of the TemporaryEnvironment.
+        It copies packages and logs into the results directory.
+
+        :param results_dir: absolute path to results directory
+        :return:
+        """
+        os.makedirs(results_dir)
+        # copy logs
+        for log in PathHelper.find_all_files(kwargs[self.TEMPDIR], '*.log'):
+            logger.debug("BuildTemporaryEnvironment: Copying log '{0}' to '{1}'".format(log, results_dir))
+            shutil.copy(log, results_dir)
+        # copy packages
+        for package in PathHelper.find_all_files(kwargs[self.TEMPDIR], '*.rpm'):
+            logger.debug("BuildTemporaryEnvironment: Copying package '{0}' to '{1}'".format(package, results_dir))
+            shutil.copy(package, results_dir)
 
 
 class BuildToolBase(object):
