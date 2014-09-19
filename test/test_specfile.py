10eb5c676ed728dbbc6ac0bf924a328cc0bb4e11
Petr Hracek <phracek@redhat.com>
2014-09-19 13:50:40 +0200

    Add man page directive

--- test/test_specfile.py
+++ test/test_specfile.py
@@ -111,12 +111,14 @@ class TestSpecFile(BaseTest):
                      '/usr/sbin/binary2',
                      '/usr/include/header.h',
                      '/usr/lib/library1.so',
-                     '/usr/lib64/library2.so']
+                     '/usr/lib64/library2.so',
+                     '/usr/share/man/man1/test.1.gz']
         expected_paths = set(['%{_bindir}/binary1',
                               '%{_sbindir}/binary2',
                               '%{_includedir}/header.h',
                               '%{_libdir}/library1.so',
-                              '%{_libdir}/library2.so'])
+                              '%{_libdir}/library2.so',
+                              '%{_mandir}/man1/test.1.gz'])
         paths = SpecFile.get_paths_with_rpm_macros(raw_paths)
         assert len(set(paths)) == len(expected_paths)
         assert len(expected_paths.intersection(set(paths))) == len(expected_paths)
