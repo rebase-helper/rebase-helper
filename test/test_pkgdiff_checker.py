ab87845c7f8a1a50345f4cf3a52afbf348ccefd0
Petr Hracek <phracek@redhat.com>
2014-11-13 14:36:44 +0100

    Fix pkg_diff tests

--- test/test_pkgdiff_checker.py
+++ test/test_pkgdiff_checker.py
@@ -39,16 +39,18 @@ class TestPkgDiff(BaseTest):
                 'changed': ['/usr/include/test.h']}
         return data
 
-    def test_pkgdiff_dictionary(self):
+    def test_fill_dictionary(self):
         expected_dict = {'added': ['/usr/sbin/test',
+                                   '/usr/sbin/pkg-1.0.2/binary_test',
                                    '/usr/lib64/libtest.so',
                                    '/usr/lib64/libtest.so.1'],
                          'removed': ['/usr/sbin/my_test',
+                                     '/usr/sbin/pkg-1.0.1/binary_test',
                                      '/usr/lib64/libtest2.so',
                                      '/usr/lib64/libtest2.so.1'],
                          'changed': ['/usr/share/test.man'],
                          'moved': ['/usr/local/test.sh',
-                                   '/usr/sbin/pkg-1.0.1/binary_test;/usr/sbin/pkg-1.0.2/binary_test'],
+                                   '/usr/sbin/pkg-1.0.1/binary_test;/usr/sbin/pkg-1.0.2/binary_test (1%)'],
                          'renamed': ['/usr/lib/libtest3.so.3',
                                      '/usr/lib/libtest3.so']}
         pdt = PkgDiffTool()
@@ -56,11 +58,20 @@ class TestPkgDiff(BaseTest):
         res_dict = pdt.fill_dictionary(pdt.results_dir)
         assert res_dict == expected_dict
 
-    def test_pkgdiff_parse_report(self):
-        expected_dict = {'moved': ['/usr/src/debug/iproute2-3.12.0/test/mytest.c'],
-                         'changed': ['/usr/include/test.h']}
+    def test_process_xml(self):
+        expected_dict = {'added': ['/usr/sbin/test',
+                                   '/usr/lib64/libtest.so',
+                                   '/usr/lib64/libtest.so.1'],
+                         'changed': ['/usr/share/test.man'],
+                         'moved': ['/usr/local/test.sh',
+                                   '/usr/sbin/pkg-1.0.1/binary_test;/usr/sbin/pkg-1.0.2/binary_test (1%)'],
+                         'removed': ['/usr/sbin/my_test',
+                                     '/usr/lib64/libtest2.so',
+                                     '/usr/lib64/libtest2.so.1'],
+                         'renamed': ['/usr/lib/libtest3.so.3',
+                                     '/usr/lib/libtest3.so']}
         pdt = PkgDiffTool()
         pdt.results_dir = self.TEST_FILES_DIR
-        res_dict = pdt.update_changed_moved(pdt.results_dir, self.get_data())
+        res_dict = pdt.process_xml_results(pdt.results_dir)
         assert res_dict == expected_dict
 
