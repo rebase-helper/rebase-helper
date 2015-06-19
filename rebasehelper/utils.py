8f28b381828b7af73d8277c43c0c80e4d7f84ae0
Petr Hracek <phracek@redhat.com>
2015-06-19 14:47:28 +0200

    Fixes #106 Replace yum with dnf.
    Signed-off-by: Petr Hracek <phracek@redhat.com>

--- rebasehelper/utils.py
+++ rebasehelper/utils.py
@@ -450,7 +450,7 @@ class RpmHelper(object):
         :param spec_path: absolute path to SPEC file
         :return:
         """
-        cmd = ['pkexec', 'yum-builddep', spec_path]
+        cmd = ['pkexec', 'dnf builddep', spec_path]
         if assume_yes:
             cmd.append('-y')
         return ProcessHelper.run_subprocess(cmd)
