c04a1c4f5b440d96791e078d1838e3111ef5032a
Nikola Forró <nforro@redhat.com>
2015-10-01 16:25:14 +0200

    Fix e-mail address configuration option, git uses "user.email"

--- rebasehelper/specfile.py
+++ rebasehelper/specfile.py
@@ -978,10 +978,10 @@ class SpecFile(object):
         new_record = []
         today = date.today()
         git_name = git_helper.command_config('--get', 'user.name')
-        git_mail = git_helper.command_config('--get', 'user.mail')
+        git_email = git_helper.command_config('--get', 'user.email')
         new_record.append('* {day} {name} <{email}> - {ver}\n'.format(day=today.strftime('%a %b %d %Y'),
                                                                       name=git_name,
-                                                                      email=git_mail,
+                                                                      email=git_email,
                                                                       ver=self.get_version()))
         new_record.append('- New upstream release {rel}\n'.format(rel=self.get_version()))
         new_record.append('\n')
