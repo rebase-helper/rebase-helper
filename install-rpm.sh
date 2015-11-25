c992f6ebf2da83d746f13253c461d61340c962d1
Nikola Forró <nforro@redhat.com>
2015-11-25 09:37:02 +0100

    Use tox for running tests

--- install-rpm.sh
+++ install-rpm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 package="rpm-bundle"
-python_modules="/usr/src/rpm-python"
 raw_github_url="https://raw.githubusercontent.com/nforro/$package/master"
 gpg_key="$raw_github_url/repo/pubkey.gpg"
 repository="deb $raw_github_url/repo/ /"
@@ -17,9 +16,6 @@ sudo apt-get -qq update || exit 1
 
 # install package
 sudo apt-get -y install "$package" || exit 1
-
-# build and install python modules
-pip install "$python_modules" || exit 1
 
 # patch test_utils.py to remove rpm tests
 patch -p1 -i rpm_test.patch
