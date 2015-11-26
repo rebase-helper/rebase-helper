fb4532e117b0ab243a50713855d9058f96ec3004
Nikola Forró <nforro@redhat.com>
2015-11-26 10:13:18 +0100

    Use readthedocs theme for local documentation builds

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,13 @@ sys.path.insert(0, rebase_helper_dir)
 
 from rebasehelper import version as rh_version
 
+# Determine if this script is running inside RTD build environment
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+    # Use RTD theme when building locally
+    import sphinx_rtd_theme
+
 
 # -- General configuration ------------------------------------------------
 
@@ -109,7 +116,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'default' if on_rtd else 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -117,7 +124,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [] if on_rtd else [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
