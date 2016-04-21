rebase-helper/rebase-helper

commit 087ff528aefbaec2257aae0f76b4abc8f130be1d

We do not want to have unapplied patches in deleted patches.
Signed-off-by: Petr Hracek <phracek@redhat.com>

rebasehelper/patch_helper.py
@@ -55,6 +55,7 @@ class PatchBase(object):
         """Method will check all patches in relevant package"""
         return NotImplementedError()
 
+
 @register_patch_tool
 class GitPatchTool(PatchBase):
 
@@ -130,7 +131,7 @@ class GitPatchTool(PatchBase):
         updated_patches = []
         for patch in cls.patches:
             patch_name = patch.get_patch_name()
-            if not [x for x in cls.output_data if patch_name in x] and patch_name not in deleted_patches:
+            if [x for x in cls.output_data if patch_name in x] and patch_name not in deleted_patches:
                 updated_patches.append(patch_name)
         return updated_patches
 

commit 0d0527e6ef6f436f7544b28bc5f33bb16c68f01c

Do not list unapplied patches in deleted patches
Attempt to do this in commit 087ff52 caused applicable patches
to be marked as deleted and vice versa.

Signed-off-by: Nikola Forró <nforro@redhat.com>

rebasehelper/patch_helper.py
@@ -125,13 +125,15 @@ class GitPatchTool(PatchBase):
         cls.output_data = cls.git_helper.get_output_data()
 
     @classmethod
-    def _update_deleted_patches(cls, deleted_patches):
+    def _update_deleted_patches(cls, deleted_patches, unapplied_patches):
         """Function checks patches against rebase-patches"""
         cls.output_data = cls.git_helper.command_log(parameters='--pretty=oneline')
         updated_patches = []
         for patch in cls.patches:
             patch_name = patch.get_patch_name()
-            if [x for x in cls.output_data if patch_name in x] and patch_name not in deleted_patches:
+            if (not [x for x in cls.output_data if patch_name in x] and
+                    patch_name not in deleted_patches and
+                    patch_name not in unapplied_patches):
                 updated_patches.append(patch_name)
         return updated_patches
 
@@ -203,7 +205,8 @@ class GitPatchTool(PatchBase):
                 cls._get_git_helper_data()
             else:
                 break
-        deleted_patches = cls._update_deleted_patches(deleted_patches)
+        deleted_patches = cls._update_deleted_patches(deleted_patches,
+                                                      unapplied_patches)
         if deleted_patches:
             patch_dictionary['deleted'] = deleted_patches
         if modified_patches: