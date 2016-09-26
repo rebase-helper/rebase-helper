17df1c12eb9a8d72546b20981c78df1ba2f02751
Nikola Forró <nforro@redhat.com>
2016-09-26 13:08:26 +0200

    Change copyright in docs to Red Hat
    Signed-off-by: Nikola Forró <nforro@redhat.com>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'rebase-helper'
-copyright = u'2016, Petr Hracek, Tomas Hozza'
+copyright = u'2014-2016, Red Hat'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
