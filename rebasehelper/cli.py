50d9a3e60c8f9d2dfbdc8ede99a92736df7e9fa3
FrNecas <fifinecas@seznam.cz>
2017-11-13 20:09:09 +0100

    Move definitions of program options to a separate file
    Signed-off-by: FrNecas <fifinecas@seznam.cz>

--- rebasehelper/cli.py
+++ rebasehelper/cli.py
@@ -27,15 +27,12 @@ import sys
 
 import six
 
+from rebasehelper.options import OPTIONS, traverse_options
 from rebasehelper.constants import PROGRAM_DESCRIPTION, NEW_ISSUE_LINK
 from rebasehelper.version import VERSION
 from rebasehelper.application import Application
 from rebasehelper.logger import logger, LoggerHelper
 from rebasehelper.exceptions import RebaseHelperError
-from rebasehelper.build_helper import Builder, SRPMBuilder
-from rebasehelper.checker import checkers_runner
-from rebasehelper.output_tool import BaseOutputTool
-from rebasehelper.versioneer import versioneers_runner
 from rebasehelper.utils import KojiHelper, ConsoleHelper
 
 
@@ -117,163 +114,29 @@ class CLI(object):
     def build_parser():
         parser = CustomArgumentParser(description=PROGRAM_DESCRIPTION,
                                       formatter_class=CustomHelpFormatter)
-        parser.add_argument(
-            "--version",
-            default=False,
-            action="store_true",
-            help="show rebase-helper version and exit"
-        )
-        parser.add_argument(
-            "-v",
-            "--verbose",
-            default=False,
-            action="store_true",
-            help="be more verbose (recommended)"
-        )
+
         group = parser.add_mutually_exclusive_group()
-        group.add_argument(
-            "-b",
-            "--build-only",
-            default=False,
-            action="store_true",
-            help="only build SRPM and RPMs"
-        )
-        group.add_argument(
-            "--comparepkgs-only",
-            default=False,
-            dest="comparepkgs",
-            metavar="COMPAREPKGS_DIR",
-            help="compare already built packages, %(metavar)s must be a directory "
-                 "with the following structure: <dir_name>/{old,new}/RPM"
-        )
-        group.add_argument(
-            "-p",
-            "--patch-only",
-            default=False,
-            action="store_true",
-            help="only apply patches"
-        )
-        parser.add_argument(
-            "--buildtool",
-            choices=Builder.get_supported_tools(),
-            default=Builder.get_default_tool(),
-            help="build tool to use, defaults to %(default)s"
-        )
-        parser.add_argument(
-            "--srpm-buildtool",
-            choices=SRPMBuilder.get_supported_tools(),
-            default=SRPMBuilder.get_default_tool(),
-            help="SRPM build tool to use, defaults to %(default)s"
-        )
-        parser.add_argument(
-            "--pkgcomparetool",
-            choices=checkers_runner.get_supported_tools(),
-            default=checkers_runner.get_default_tools(),
-            type=lambda s: s.split(','),
-            help="set of tools to use for package comparison, defaults to %(default)s"
-        )
-        parser.add_argument(
-            "--outputtool",
-            choices=BaseOutputTool.get_supported_tools(),
-            default=BaseOutputTool.get_default_tool(),
-            help="tool to use for formatting rebase output, defaults to %(default)s"
-        )
-        parser.add_argument(
-            "--versioneer",
-            choices=versioneers_runner.get_available_versioneers(),
-            default=None,
-            help="tool to use for determining latest upstream version"
-        )
-        parser.add_argument(
-            "--not-download-sources",
-            default=False,
-            action="store_true",
-            help="do not download sources"
-        )
-        parser.add_argument(
-            "-w",
-            "--keep-workspace",
-            default=False,
-            action="store_true",
-            help="do not remove workspace directory after finishing"
-        )
-        parser.add_argument(
-            "-c",
-            "--continue",
-            default=False,
-            action="store_true",
-            dest='cont',
-            help="continue previously interrupted rebase"
-        )
-        parser.add_argument(
-            "--color",
-            choices=['always', 'never', 'auto'],
-            default='auto',
-            help="colorize the output, defaults to %(default)s"
-        )
-        parser.add_argument(
-            "--disable-inapplicable-patches",
-            default=False,
-            action="store_true",
-            dest='disable_inapplicable_patches',
-            help="disable inapplicable patches in rebased SPEC file"
-        )
-        parser.add_argument(
-            "--non-interactive",
-            default=False,
-            action="store_true",
-            dest='non_interactive',
-            help="do not interact with user"
-        )
-        parser.add_argument(
-            "--build-tasks",
-            dest="build_tasks",
-            metavar="OLD_TASK,NEW_TASK",
-            type=lambda s: s.split(','),
-            help="comma-separated remote build task ids"
-        )
-        parser.add_argument(
-            "--builds-nowait",
-            default=False,
-            action="store_true",
-            help="do not wait for remote builds to finish"
-        )
-        parser.add_argument(
-            "--builder-options",
-            default=None,
-            metavar="BUILDER_OPTIONS",
-            help="enable arbitrary local builder option(s), enclose %(metavar)s in quotes "
-                 "and note that = before it is mandatory"
-        )
-        parser.add_argument(
-            "--srpm-builder-options",
-            default=None,
-            metavar="SRPM_BUILDER_OPTIONS",
-            help="enable arbitrary local srpm builder option(s), enclose %(metavar)s in quotes "
-                 "and note that = before it is mandatory"
-        )
-        parser.add_argument(
-            "--results-dir",
-            help="directory where rebase-helper output will be stored"
-        )
-        parser.add_argument(
-            "--get-old-build-from-koji",
-            default=False,
-            action="store_true",
-            help="do not build old sources, download latest build from Koji instead"
-        )
-        parser.add_argument(
-            "sources",
-            metavar='SOURCES',
-            nargs='?',
-            default=None,
-            help="new upstream sources"
-        )
-        parser.add_argument(
-            "--changelog-entry",
-            default="- New upstream release %{version}",
-            help="text to use as changelog entry, can contain RPM macros, which will be expanded"
-        )
+        current_group = 0
+        for option in traverse_options(OPTIONS):
+            option_kwargs = dict(option)
+            for key in ("group", "default", "name"):
+                if key in option_kwargs:
+                    del option_kwargs[key]
+
+            if "group" in option:
+                if current_group != option["group"]:
+                    current_group = option["group"]
+                    group = parser.add_mutually_exclusive_group()
+                actions_container = group
+            else:
+                actions_container = parser
+
+            # default is set to SUPPRESS to prevent arguments which were not specified on command line from being
+            # added to namespace. This allows rebase-helper to determine which arguments are used with their
+            # default value. This is later used for merging CLI arguments with config.
+            actions_container.add_argument(*option["name"], action=CustomAction, default=argparse.SUPPRESS,
+                                           actual_default=option.get("default"), **option_kwargs)
+
         return parser
 
     def __init__(self, args=None):
---
+++ rebasehelper/options.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+#
+# This tool helps you to rebase package to the latest version
+# Copyright (C) 2013-2014 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# he Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Authors: Petr Hracek <phracek@redhat.com>
+#          Tomas Hozza <thozza@redhat.com>
+
+from rebasehelper.build_helper import Builder, SRPMBuilder
+from rebasehelper.checker import checkers_runner
+from rebasehelper.output_tool import BaseOutputTool
+from rebasehelper.versioneer import versioneers_runner
+
+OPTIONS = [
+    {
+        "name": ("--version",),
+        "default": False,
+        "switch": True,
+        "help": "show rebase-helper version and exit",
+    },
+    {
+        "name": ("-v", "--verbose"),
+        "default": False,
+        "switch": True,
+        "help": "be more verbose (recommended)",
+    },
+    [
+        {
+            "name": ("-b", "--build-only"),
+            "default": False,
+            "switch": True,
+            "help": "only build SRPM and RPMs",
+        },
+        {
+            "name": ("--comparepkgs-only",),
+            "default": False,
+            "dest": "comparepkgs",
+            "metavar": "COMPAREPKGS_DIR",
+            "help": "compare already built packages, %(metavar)s must be a directory "
+                    "with the following structure: <dir_name>/{old,new}/RPM",
+        },
+        {
+            "name": ("-p", "--patch-only"),
+            "default": False,
+            "switch": True,
+            "help": "only apply patches",
+        },
+    ],
+    {
+        "name": ("--buildtool",),
+        "choices": Builder.get_supported_tools(),
+        "default": Builder.get_default_tool(),
+        "help": "build tool to use, defaults to %(default)s",
+    },
+    {
+        "name": ("--srpm-buildtool",),
+        "choices": SRPMBuilder.get_supported_tools(),
+        "default": SRPMBuilder.get_default_tool(),
+        "help": "SRPM build tool to use, defaults to %(default)s",
+    },
+    {
+        "name": ("--pkgcomparetool",),
+        "choices": checkers_runner.get_supported_tools(),
+        "default": checkers_runner.get_default_tools(),
+        "type": lambda s: s.split(','),
+        "help": "set of tools to use for package comparison, defaults to %(default)s",
+    },
+    {
+        "name": ("--outputtool",),
+        "choices": BaseOutputTool.get_supported_tools(),
+        "default": BaseOutputTool.get_default_tool(),
+        "help": "tool to use for formatting rebase output, defaults to %(default)s",
+    },
+    {
+        "name": ("--versioneer",),
+        "choices": versioneers_runner.get_available_versioneers(),
+        "default": None,
+        "help": "tool to use for determining latest upstream version",
+    },
+    {
+        "name": ("--not-download-sources",),
+        "default": False,
+        "switch": True,
+        "help": "do not download sources",
+    },
+    {
+        "name": ("-w", "--keep-workspace"),
+        "default": False,
+        "switch": True,
+        "help": "do not remove workspace directory after finishing",
+    },
+    {
+        "name": ("-c", "--continue"),
+        "default": False,
+        "switch": True,
+        "dest": "cont",
+        "help": "continue previously interrupted rebase",
+    },
+    {
+        "name": ("--color",),
+        "choices": ["always", "never", "auto"],
+        "default": "auto",
+        "help": "colorize the output, defaults to %(default)s",
+    },
+    {
+        "name": ("--disable-inapplicable-patches",),
+        "default": False,
+        "switch": True,
+        "dest": "disable_inapplicable_patches",
+        "help": "disable inapplicable patches in rebased SPEC file",
+    },
+    {
+        "name": ("--non-interactive",),
+        "default": False,
+        "switch": True,
+        "dest": "non_interactive",
+        "help": "do not interact with user",
+    },
+    {
+        "name": ("--build-tasks",),
+        "dest": "build_tasks",
+        "metavar": "OLD_TASK,NEW_TASK",
+        "type": lambda s: s.split(','),
+        "help": "comma-separated remote build task ids",
+    },
+    {
+        "name": ("--builds-nowait",),
+        "default": False,
+        "switch": True,
+        "help": "do not wait for remote builds to finish",
+    },
+    {
+        "name": ("--builder-options",),
+        "default": None,
+        "metavar": "BUILDER_OPTIONS",
+        "help": "enable arbitrary local builder option(s), enclose %(metavar)s in quotes "
+                "and note that = before it is mandatory",
+    },
+    {
+        "name": ("--srpm-builder-options",),
+        "default": None,
+        "metavar": "SRPM_BUILDER_OPTIONS",
+        "help": "enable arbitrary local srpm builder option(s), enclose %(metavar)s in quotes "
+                "and note that = before it is mandatory",
+    },
+    {
+        "name": ("--build-retries",),
+        "default": 2,
+        "help": "number of retries of a failed build, defaults to %(default)d",
+        "type": int,
+    },
+    {
+        "name": ("--results-dir",),
+        "help": "directory where rebase-helper output will be stored",
+    },
+    {
+        "name": ("--get-old-build-from-koji",),
+        "default": False,
+        "switch": True,
+        "help": "do not build old sources, download latest build from Koji instead",
+    },
+    {
+        "name": ("sources",),
+        "metavar": "SOURCES",
+        "nargs": "?",
+        "default": None,
+        "help": "new upstream sources",
+    },
+    {
+        "name": ("--changelog-entry",),
+        "default": "- New upstream release %{version}",
+        "help": "text to use as changelog entry, can contain RPM macros, which will be expanded",
+    },
+]
+
+
+def traverse_options(options):
+    group_index = 0
+    for opt in options:
+        if isinstance(opt, list):
+            for inner_opt in opt:
+                yield dict(group=group_index, **inner_opt)
+            group_index += 1
+        else:
+            yield opt
