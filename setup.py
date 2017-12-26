cafe8e5343e0437c6afce1e2fdf290cadf6bcfae
FrNecas <fifinecas@seznam.cz>
2017-12-26 21:29:00 +0100

    Fix requirements indentation
    Signed-off-by: FrNecas <fifinecas@seznam.cz>

--- setup.py
+++ setup.py
@@ -47,13 +47,13 @@ def get_rpm_distribution():
 
 def get_requirements():
     result = [
-       'backports.lzma;python_version<"3.3"',
-       'copr',
-       'pyquery',
-       'requests',
-       'six',
-       'GitPython',
-       'ansicolors',
+        'backports.lzma;python_version<"3.3"',
+        'copr',
+        'pyquery',
+        'requests',
+        'six',
+        'GitPython',
+        'ansicolors',
     ]
     # there is no rpm inside RTD build environment
     if not os.getenv('READTHEDOCS'):
