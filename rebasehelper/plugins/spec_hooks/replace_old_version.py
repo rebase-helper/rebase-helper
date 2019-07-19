352bfa98c1deb5561a1b70d13475177d01011ec3
František Nečas <fifinecas@seznam.cz>
2019-07-19 12:31:42 +0200

    Try to replace parts of version in Patches and Sources
    Also fix the regex to allow for replacement in macro definitions.
    
    Signed-off-by: František Nečas <fifinecas@seznam.cz>

--- rebasehelper/plugins/spec_hooks/replace_old_version.py
+++ rebasehelper/plugins/spec_hooks/replace_old_version.py
@@ -25,8 +25,11 @@
 import re
 import urllib.parse
 
+from typing import Any, List, Pattern, Tuple
+
 from rebasehelper.types import Options
 from rebasehelper.plugins.spec_hooks import BaseSpecHook
+from rebasehelper.specfile import SpecFile
 
 
 class ReplaceOldVersion(BaseSpecHook):
@@ -58,22 +61,58 @@ class ReplaceOldVersion(BaseSpecHook):
         return not urllib.parse.urlparse(source).scheme
 
     @classmethod
-    def run(cls, spec_file, rebase_spec_file, **kwargs):
+    def _create_possible_replacements(cls, old: str, new: str, use_macro: bool) -> List[Tuple[Pattern[str], str]]:
+        """Creates possible subversion replacements.
+
+        Args:
+            old: Old version.
+            new: New Version.
+            use_macro: Whether %{version} macro should be used as a replacement.
+
+        Returns:
+            List of tuples containing regex pattern and replacement
+            that can be passed to re.sub. The first tuple always
+            represents the whole version.
+
+        Example:
+            Subversions 1.2, 1.2.3, 1.2.3.4 would be created from version 1.2.3.4.
+
+        """
+        version_re = r'([\ /\-\s]){}([/.\-\s]|$)'
+        replacement = r'\g<1>{}\g<2>'
+        split_version = old.split('.')
+
+        res = [
+            (re.compile(version_re.format(re.escape(old))), replacement.format('%{version}' if use_macro else new))
+        ]
+        # iterate backwards to go from longer to shorter subversions
+        for i in reversed(range(2, len(split_version))):
+            pattern = re.compile(version_re.format(re.escape('.'.join(split_version[:i]))))
+            new_subversion = replacement.format('.'.join(new.split('.')[:i]))
+            res.append((pattern, new_subversion))
+        return res
+
+    @classmethod
+    def run(cls, spec_file: SpecFile, rebase_spec_file: SpecFile, **kwargs: Any):
         old_version = spec_file.get_version()
         new_version = rebase_spec_file.get_version()
-        replace_with_macro = kwargs.get('replace_old_version_with_macro')
-        pattern = re.compile(r'([/\-\s]){}([/.\-\s])'.format(re.escape(old_version)))
+        replace_with_macro = bool(kwargs.get('replace_old_version_with_macro'))
+
+        subversion_patterns = cls._create_possible_replacements(old_version, new_version, replace_with_macro)
         for sec_name, section in rebase_spec_file.spec_content.sections:
             if sec_name.startswith('%changelog'):
                 continue
             for index, line in enumerate(section):
                 # special case Version tag to avoid mistakes when version format changes
                 # e.g. in a rebase from 2.5 to 2.5.1, version would be changed to 2.5.1.1
-                if cls._is_local_source(line) or line.startswith('Version:'):
+                if cls._is_local_source(line) or line.startswith('Version'):
                     continue
                 start, end = spec_file.spec_content.get_comment_span(line, sec_name)
-                replacement = r'\g<1>' + ('%{version}' if replace_with_macro else new_version) + r'\g<2>'
-                updated_line = pattern.sub(replacement, line[:start])
+                # try to replace the whole version first
+                updated_line = subversion_patterns[0][0].sub(subversion_patterns[0][1], line)
+                if (line.startswith('Patch') or line.startswith('Source')) and urllib.parse.urlparse(line.split()[1]):
+                    for sub_pattern, repl in subversion_patterns[1:]:
+                        updated_line = sub_pattern.sub(repl, updated_line)
                 section[index] = updated_line + line[start:end]
 
         rebase_spec_file.save()
