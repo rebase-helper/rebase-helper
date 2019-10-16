3009bfcb1a1e185d7b46211477f8ae1f9a2b6ddc
Nikola Forró <nforro@redhat.com>
2019-10-16 11:44:08 +0200

    Do not consider URL to be a commented-out tag
    It's quite common to add comments with e.g. bugzilla URLs before Patch
    tags in a SPEC. These should of course be removed when removing
    the tag. Enhance the tag pattern not to match "http(s)://".
    
    Signed-off-by: Nikola Forró <nforro@redhat.com>

--- rebasehelper/specfile.py
+++ rebasehelper/specfile.py
@@ -577,7 +577,7 @@ class SpecFile:
             disable_inapplicable: Whether to comment out inapplicable patches.
         """
         def is_comment(line):
-            if re.match(r'^#\s*[A-Za-z][A-Za-z0-9]+\s*:', line):
+            if re.match(r'^#\s*[A-Za-z][A-Za-z0-9]+\s*:(?!//)', line):
                 # ignore commented-out tag
                 return False
             return line.startswith('#')
