14b65e4758b679c0c1c25eb7f199af8e29e0542e
František Nečas <fifinecas@seznam.cz>
2020-01-28 14:02:47 +0100

    Add support for automatic patch/source numbering
    Signed-off-by: František Nečas <fifinecas@seznam.cz>

--- rebasehelper/tests/test_tags.py
+++ rebasehelper/tests/test_tags.py
@@ -37,7 +37,9 @@ Version: 0.1
 Source: test.tar.xz
 SOURCE1: additional-files.zip
 Patch00: patch0000.diff
-Patch01: patch0001.diff
+Patch:   patch0001.diff
+Patch:   patch0002.diff
+Patch03: patch0003.diff
 %{?!optimize:Patch999: extras.patch}
 %{?optimize:Patch999: extras-optimized.patch}
 
@@ -51,7 +53,7 @@ list.patch
 %Package libs
 Requires: %{name}-%{version}
 Requires: extradep
-Source3: source3.tar.gz
+Source: source3.tar.gz
 
 %sourcelist
 source4.zip
@@ -68,7 +70,10 @@ Version: 0.1
 Source: test.tar.xz
 SOURCE1: additional-files.zip
 Patch00: patch0000.diff
-Patch01: patch0001.diff
+Patch:   patch0001.diff
+Patch:   patch0002.diff
+Patch03: patch0003.diff
+
 Patch999: extras-optimized.patch
 
 %sourcelist
@@ -81,7 +86,7 @@ list.patch
 %Package libs
 Requires: test-0.1
 Requires: extradep
-Source3: source3.tar.gz
+Source: source3.tar.gz
 
 %sourcelist
 source4.zip
@@ -102,25 +107,27 @@ source4.zip
         return Tags(SpecContent(self.RAW_CONTENT), SpecContent(self.PARSED_CONTENT))
 
     def test_tags(self, tags):
-        assert len(tags) == 14
+        assert len(tags) == 16
         assert tags[0] == Tag(0, '%package', 1, 'Name', (6, 10), True)
         assert tags[1] == Tag(0, '%package', 2, 'Version', (9, 12), True)
-        assert tags[2] == Tag(0, '%package', 3, 'Source0', (8, 19), True)
-        assert tags[3] == Tag(0, '%package', 4, 'Source1', (9, 29), True)
-        assert tags[4] == Tag(0, '%package', 5, 'Patch0', (9, 23), True,)
-        assert tags[5] == Tag(0, '%package', 6, 'Patch1', (9, 23), True)
-        assert tags[6] == Tag(0, '%package', 8, 'Patch999', (22, 44), True)
-        assert tags[7] == Tag(1, '%sourcelist', 1, 'Source2', (0, 11), True)
-        assert tags[8] == Tag(2, '%patchlist', 0, 'Patch1000', (0, 10), True)
-        assert tags[9] == Tag(3, '%package libs', 0, 'Requires', (10, 28), True)
-        assert tags[10] == Tag(3, '%package libs', 1, 'Requires', (10, 18), True)
-        assert tags[11] == Tag(3, '%package libs', 2, 'Source3', (9, 23), True)
-        assert tags[12] == Tag(4, '%sourcelist', 0, 'Source4', (0, 11), True)
-        assert tags[13] == Tag(5, '%package utils', 0, 'Requires', (10, 28), False)
+        assert tags[2] == Tag(0, '%package', 3, 'Source0', (8, 19), True, 0)
+        assert tags[3] == Tag(0, '%package', 4, 'Source1', (9, 29), True, 1)
+        assert tags[4] == Tag(0, '%package', 5, 'Patch0', (9, 23), True, 0)
+        assert tags[5] == Tag(0, '%package', 6, 'Patch1', (9, 23), True, 1)
+        assert tags[6] == Tag(0, '%package', 7, 'Patch2', (9, 23), True, 2)
+        assert tags[7] == Tag(0, '%package', 8, 'Patch3', (9, 23), True, 3)
+        assert tags[8] == Tag(0, '%package', 10, 'Patch999', (22, 44), True, 999)
+        assert tags[9] == Tag(1, '%sourcelist', 1, 'Source2', (0, 11), True, 2)
+        assert tags[10] == Tag(2, '%patchlist', 0, 'Patch1000', (0, 10), True, 1000)
+        assert tags[11] == Tag(3, '%package libs', 0, 'Requires', (10, 28), True)
+        assert tags[12] == Tag(3, '%package libs', 1, 'Requires', (10, 18), True)
+        assert tags[13] == Tag(3, '%package libs', 2, 'Source3', (8, 22), True, 3)
+        assert tags[14] == Tag(4, '%sourcelist', 0, 'Source4', (0, 11), True, 4)
+        assert tags[15] == Tag(5, '%package utils', 0, 'Requires', (10, 28), False)
 
     def test_filter(self, tags):
-        assert len(list(tags.filter(section_name='%package'))) == 7
-        assert len(list(tags.filter(section_name='%package', name='Patch*'))) == 3
+        assert len(list(tags.filter(section_name='%package'))) == 9
+        assert len(list(tags.filter(section_name='%package', name='Patch*'))) == 5
         assert len(list(tags.filter(section_name='%package', valid=False))) == 0
         assert len(list(tags.filter(valid=False))) == 1
         assert len(list(tags.filter(name='Requires', valid=None))) == 3
